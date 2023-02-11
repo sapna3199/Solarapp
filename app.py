7766092d2e8d0905f1f9bac1591c6d79500f2262
sapna3199 <68215799+sapna3199@users.noreply.github.com>
2023-02-11 22:55:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import joblib
 
 app = Flask(__name__)
 
-file = open('G:\project2\finalized_model.pkl', 'rb')
+file = open(r'G:\project2\finalized_model.pkl', 'rb') //absolute path
 model = pickle.load(file)
 file.close()
 
