3e94be6f24ed52ae8639ff0c33fa50e0c2e71b14
sapna3199 <68215799+sapna3199@users.noreply.github.com>
2023-02-11 16:19:37 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+import pandas as pd
+import numpy as np
+import pickle
+from sklearn.tree import DecisionTreeClassifier
+import joblib
+
+app = Flask(__name__)
+
+model = pickle.load(open('G:/project2/finalized_model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return(render_template('index.html'))
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    
+    # input
+    input_features = [float(x) for x in request.form.values()]
+    feature_value = np.array(input_features)
+    
+    # output
+    output = model.predict([feature_value])[0].round(0)
+    
+    if output == 0:
+        return render_template('index.html', prediction_text='Solar panel is faulty.')
+    else:
+        return render_template('index.html', prediction_text='Solar panel is nort faulty.')
+    
+    return render_template('index.html')
+
+
+
+if __name__ == '__main__':
+	app.run(debug=True,use_reloader=False)
